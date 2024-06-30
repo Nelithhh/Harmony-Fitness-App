c8169cc6e0f825ba4306c3863edf8defb2c3da46
Nelith Karunarathne <nelith550@gmail.com>
2024-07-01 03:01:34 +0530

    Hero.jsx finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      Hero
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+        <div className='flex flex-col gap-4'>
+            <p>IT'S TIME TO GET</p>
+            <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>SWOLE<span className='text-blue-400'>NORMOUS</span></h1>   
+        </div>
+      
+        <p className='text-sm md:text-base font-light'>I hereby acknowledgment that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, affiliated with severe body dismorphia, unable to fit through doors.</p>
+
+        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'><p>Accept and Begin</p></button>
     </div>
   )
 }
