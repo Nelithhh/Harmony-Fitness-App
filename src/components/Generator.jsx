4c68cd39d955456313e2f457aa878c865d854e03
Nelith Karunarathne <nelith550@gmail.com>
2024-07-01 22:36:40 +0530

    Buttons

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
+import { WORKOUTS } from '../utils/swoldier'
+
+function Header(props) {
+  const {index, title, description} = props
+
+  return(
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center justify-center gap-2'>
+        <p className='font-semibold tex-3xl sm:text-4xl md:text-5xl text-slate-400'>{index}</p>
+        <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+      </div>
+      <p className='mx-auto text-sm sm:text-base'>{description}</p>
+    </div>
+  )
+}
 
 function Generator() {
   return (
     <SectionWrapper header={"generate your workout"} title={['It\'s','Huge','o\'clock']}>
+      <Header index={'01'} title={'Pick you poison'} description={"Select the workout you wish to endure."} />
+      <div className='grid grid-cols-2 gap-4 sm:grid-cols-4'>
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return(
+            <button className='py-3 duration-200 border border-blue-400 rounded-lg bg-slate-950 hover:border-blue-600' key={typeIndex}>
+              <p className='capitalize'>{type.replaceAll('_', " ")}</p>
+            </button>
+          )
+        })}
+      </div>
 
+      <Header index={'02'} title={'Locked on targets'} description={"Select the muscles judged for annihlation."} />
+      <div className='p-3 border border-blue-400 border-solid rounded-lg bg-slate-950'>
+        <div className='relative flex items-center justify-center'>
+          <p>Select muscle group</p>
+          <i className="absolute -translate-y-1/2 fa-solid right-3 top-1/2 fa-caret-down"></i>
+        </div>
+        {}
+      </div>
     </SectionWrapper>
   )
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -6,11 +6,14 @@ function SectionWrapper(props) {
 
   return (
     <section className='flex flex-col min-h-screen gap-10'>
-        <div className='flex flex-col items-center justify-center gap-2 py-10 bg-slate-950'>
+        <div className='flex flex-col items-center justify-center gap-2 p-4 py-10 bg-slate-950'>
             <p className='font-medium uppercase'>{header}</p>
             <h2 className='text-3xl font-semibold sm:text-4xl md:text-5xl lg:text-6xl'>{title[0]} <span className='text-blue-400 uppercase'>{title[1]}</span> {title[2]}</h2>
         </div>
-        {children}
+        <div className='mas-w-[800px] w-full flex flex-col mx-auto gap-10 p-4'>
+            {children}
+        </div>
+        
     </section>
   )
 }
